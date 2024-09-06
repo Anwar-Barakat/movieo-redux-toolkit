242ac223c06d41b5532d1958c56df2a03c85563e
Anwar-Barakat <brkatanwar0@gmail.com>
2024-09-06 22:01:22 +0400

    Refactor CSS and add trending movie carousel functionality

--- src/pages/home/TrendingBanner.jsx
+++ src/pages/home/TrendingBanner.jsx
@@ -5,13 +5,11 @@ import { ClipLoader } from "react-spinners";
 
 // Import Swiper and required modules
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Navigation, Autoplay, EffectCoverflow, Zoom } from "swiper/modules";
+import { Navigation, Autoplay, EffectCoverflow, Zoom } from "swiper/modules";
 
 // Import Swiper styles
 import "swiper/css";
-import "swiper/css/pagination";
 import "swiper/css/navigation";
-import "swiper/css/effect-coverflow";
 import "swiper/css/zoom";
 import { Link } from "react-router-dom";
 
@@ -27,9 +25,9 @@ const TrendingBanner = () => {
 
     return (
         <section className="w-full">
-            <div className="lg:h-full h-[90vh]">
+            <div className="lg:h-[100vh] h-[90vh] relative">
                 {status === "loading" && (
-                    <div className="w-full flex justify-center items-center">
+                    <div className="w-full h-full flex justify-center items-center">
                         <ClipLoader color="#e53e3e" size={50} />
                     </div>
                 )}
@@ -39,48 +37,55 @@ const TrendingBanner = () => {
                         modules={[Navigation, Autoplay, EffectCoverflow, Zoom]}
                         spaceBetween={30}
                         slidesPerView={1}
-                        navigation
-                        autoplay={{ delay: 50000, disableOnInteraction: false }}
+                        navigation={{ nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" }}
+                        autoplay={{ delay: 5000, disableOnInteraction: false }}
                         loop={true}
                         speed={1000}
                         className="w-full h-full"
                     >
                         {trendingMovies.map((movie) => (
-                            <>
-                                <SwiperSlide key={movie.id} className="w-full trending-slide relative">
-                                    <div className="grid gap-2 h-full">
-                                        <img
-                                            src={imageUrl + movie.backdrop_path}
-                                            alt={movie.title}
-                                            className="w-full h-full object-cover"
-                                        />
-
-
-                                        <div className='absolute top-0 w-full bg-gradient-to-t from-neutral-900 to-transparent'></div>
-
-                                        <div className='absolute bottom-3 flex items-center justify-center text-center w-full'>
-                                            <div className='w-full grid gap-2 max-w-2xl'>
-                                                <h2 className='font-bold text-2xl lg:text-4xl text-white drop-shadow-2xl'>{movie?.title || movie?.name}</h2>
-                                                <p className='text-ellipsis line-clamp-3'>{movie.overview}</p>
-                                                <div className='flex items-center gap-4'>
-                                                    <p>Rating : {Number(movie.vote_average).toFixed(1)}+</p>
-                                                    <span>|</span>
-                                                    <p>View : {Number(movie.popularity).toFixed(0)}</p>
-                                                </div>
-                                                <Link to={"/" + movie?.media_type + "/" + movie.id}>
-                                                    <button className=' bg-white px-4 py-2 text-black font-bold rounded  hover:bg-gradient-to-l from-primary to-orange-500 shadow-md transition-all hover:scale-105'>
-                                                        Play Now
-                                                    </button>
-                                                </Link>
+                            <SwiperSlide key={movie.id} className="w-full h-full relative">
+                                <img
+                                    src={imageUrl + movie.backdrop_path}
+                                    alt={movie.title}
+                                    className="w-full h-full object-cover"
+                                    loading="lazy"
+                                />
+                                <div className="absolute inset-0 bg-gradient-to-t from-neutral-900 to-transparent"></div>
+                                <div className="absolute bottom-[10%] flex items-center justify-center text-center w-full">
+                                    <div className="w-full grid gap-4 max-w-2xl">
+                                        <h2 className="font-bold text-2xl lg:text-4xl text-white drop-shadow-2xl">
+                                            {movie?.title || movie?.name}
+                                        </h2>
+                                        <p className="text-white text-ellipsis line-clamp-3">{movie.overview}</p>
+                                        <div className="flex justify-center w-full gap-4 text-white">
+                                            <div className="flex gap-2">
+                                                <p>Rating: {Number(movie.vote_average).toFixed(1)}+</p>
+                                                <span>|</span>
+                                                <p>View: {Number(movie.popularity).toFixed(0)}</p>
                                             </div>
                                         </div>
+                                        <Link to={`/${movie?.media_type}/${movie.id}`}>
+                                            <button className="main__btn shadow-md">
+                                                Play Now
+                                            </button>
+                                        </Link>
                                     </div>
-                                </SwiperSlide>
-                            </>
-
+                                </div>
+                            </SwiperSlide>
                         ))}
                     </Swiper>
                 )}
+                <div className="swiper-button-prev custom-nav-button">
+                    <svg className="w-10 h-10 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                        <path d="M15 19l-7-7 7-7" />
+                    </svg>
+                </div>
+                <div className="swiper-button-next custom-nav-button">
+                    <svg className="w-10 h-10 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                        <path d="M9 5l7 7-7 7" />
+                    </svg>
+                </div>
             </div>
         </section>
     );
